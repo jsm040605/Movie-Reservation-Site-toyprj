5a1b3307c8af56837a6814a390fca88e4c670f4d
taeg <leetaegyoung9492@naver.com>
2025-12-10 19:51:07 +0900

    Modify: match version

--- src/main/java/com/example/theater_proj/global/common/config/WebMvcConfig.java
+++ src/main/java/com/example/theater_proj/global/common/config/WebMvcConfig.java
@@ -15,7 +15,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(authInterceptor)
-                .addPathPatterns("/api/**")
-                .excludePathPatterns("/api/auth/**");
+//                .addPathPatterns("/api/**")
+                .excludePathPatterns("/api/**");
     }
 }
